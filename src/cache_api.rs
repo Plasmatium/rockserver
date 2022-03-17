e81619fd6680985e9c50d22da2e6f98fbcf1ecde
Plasmatium <slv309@163.com>
2022-03-17 09:50:23 +0800

    add pretty json present for cache

--- src/cache_api.rs
+++ src/cache_api.rs
@@ -12,9 +12,12 @@ use crate::cache::{GLOBAL_CACHE, CacheObject, replace_global_cache};
  * POST /rockserver/cache.json
  */
 
-pub async fn get_cache_json() -> Json<&'static DashMap<String, CacheObject>> {
+pub async fn get_cache_json() -> (StatusCode, String) {
     let cache = &GLOBAL_CACHE.0;
-    Json(cache)
+    match serde_json::to_string_pretty(&cache) {
+        Ok(data) => (StatusCode::OK, data),
+        Err(e) => (StatusCode::INTERNAL_SERVER_ERROR, e.to_string().into())
+    }
 }
 
 pub async fn post_cache_json(Json(new_cache): Json<DashMap<String, CacheObject>>) -> http::StatusCode {
